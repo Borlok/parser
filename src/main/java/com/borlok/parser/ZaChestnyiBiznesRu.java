cb7fe1de9e1b140351f78e903c171a9f14b064a1
Yurii Erofeevski <borlok@bk.ru>
2021-03-24 00:37:22 +0300

    v1.1.1

--- src/main/java/com/borlok/parser/ZaChestnyiBiznesRu.java
+++ src/main/java/com/borlok/parser/ZaChestnyiBiznesRu.java
@@ -3,6 +3,7 @@ package com.borlok.parser;
 import com.borlok.model.Company;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
@@ -12,20 +13,26 @@ import java.util.stream.Collectors;
 public class ZaChestnyiBiznesRu {
     private static final String startUrl = "https://zachestnyibiznes.ru";
     private static final Set<Company> companies = new HashSet<>();
+    private static int companyNumber; //TODO вконце удалить
 
     public static void parse() throws IOException {
-//        String inputUrl = startUrl + "/search?query=респ+Татарстан";
-//
-//        Document document = Jsoup
-//                .connect(inputUrl)
-//                .headers(fillHeaders())
-//                .get();
-//
-//        Elements itemProp = document.select("a[itemprop]");
-//        for (Element element : itemProp)
-//            createCompany(element.attr("href"));
+        int count = 2;
+        int i = 1;
+        while (i != count) {
 
-        createCompany("/company/ul/1041621022574_1655083578_UPRAVLENIE-ROSKOMNADZORA-PO-RESPUBLIKE-TATARSTAN-TATARSTAN");
+            String inputUrl = startUrl + "/search?query=респ+Татарстан&page=" + i;
+
+            Document document = Jsoup
+                    .connect(inputUrl)
+                    .headers(fillHeaders())
+                    .get();
+
+            Elements itemProp = document.select("a[itemprop]");
+            for (Element element : itemProp)
+                createCompany(element.attr("href"));
+            i++;
+        }
+//        createCompany("/company/ul/1051622166848_1655102728_OOO-ROSINTER-RESTORANTS-TATARSTAN");
 
         for (Company company : companies)
             System.out.println(company);
@@ -56,15 +63,14 @@ public class ZaChestnyiBiznesRu {
             String date = getDateFromElement(registerDateElement);
             int inn = Integer.parseInt(getStringFromElement(innElement));
             String address = getAddress(addressElement);
-            System.out.print(href + " ");
-            String director = getDirectorsName(directorNameElement);
+//            System.out.print(href + " "); //TODO
+            String director = getDirectorsName(directorNameElement, document);
             int foundersNumber = Integer.parseInt(founderElementNum.first().text());
-            List<String> founders = getFounders(document,foundersElement, foundersNumber);
-            System.out.print(director + " ");
+            List<String> founders = getFounders(document, foundersElement, foundersNumber);
+//            System.out.println(director + " ");
+//            System.out.println(); //TODO
 
 
-
-            System.out.println(" Соответствие соочередителей: " + (founders.size() == foundersNumber));
             company = new Company(inn, director, founders, address, companyName, date, status, "UNKNOWN");
         } catch (Exception e) {
             return;
@@ -73,17 +79,31 @@ public class ZaChestnyiBiznesRu {
     }
 
     private static List<String> getFounders(Document document, Elements foundersElement, int foundersNumber) {
-        List<String> founders = new ArrayList<>();
-
+        Set<String> founders = new HashSet<>();
         if (foundersNumber != 0) {
-            if (foundersElement.textNodes().size() <= 0) {
+            for (int i = 0; i < foundersElement.textNodes().size(); i++)
+                founders.add(foundersElement.get(i).text());
+            if (foundersElement.textNodes().isEmpty() || foundersElement.textNodes().size() != foundersNumber) {
                 foundersElement = document.select("table.hidden-print").select("a[target]");
                 for (int i = 0; i < foundersElement.textNodes().size(); i = i + 2)
                     founders.add(foundersElement.get(i).text());
-            } else
-                return foundersElement.stream().map(x -> x + "").collect(Collectors.toList());
+            }
+            if (founders.isEmpty() || founders.size() != foundersNumber) {
+                //TODO здесь объединить на конечном этапе
+                foundersElement = document.select("table.hidden-print").select("td");
+                founders = foundersElement.textNodes().stream()
+                        .filter(x -> !(x.text().matches("По данным портала ЗАЧЕСТНЫЙБИЗНЕС"))
+                                && !x.text().equals(" ")
+                                && !x.text().equals("")
+                                && !x.text().matches("\\d*%")
+                                && !x.text().matches("\\d*"))
+                        .map(x -> "" + x.text().trim())
+                        .collect(Collectors.toSet());
+            }
         }
-        return founders;
+        System.out.print(++companyNumber + " Соответствие соочередителей: " + foundersNumber + " " + (founders.size() == foundersNumber) + " "); //TODO
+        System.out.println(); //TODO
+        return new ArrayList<>(founders);
     }
 
     private static String getAddress(Elements addressElement) {
@@ -98,12 +118,16 @@ public class ZaChestnyiBiznesRu {
         return element.text();
     }
 
-    private static String getDirectorsName(Elements directorNameElement) {
+    private static String getDirectorsName(Elements directorNameElement, Document document) {
         //TODO Сделать что нибудь с компанией вместо физ лица
         try {
             return directorNameElement.get(0).text();
         } catch (IndexOutOfBoundsException e) {
-            return  "Company";
+            //TODO здесь объединить на конечном этапе
+            Elements elements = document
+                    .select("div.m-t-15:contains(Управляющая организация)")
+                    .select("a[target=_blank]");
+            return elements.textNodes().get(0).text();
         }
     }
 
